6e76bb3cbf502cf84fad182246bf95ce2799ab69
Zachary P <zachary.s.prince@outlook.com>
2022-07-12 15:23:11 -0400

    Add merchants controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,3 +1,5 @@
-class MerchantsController < ApplicationController
-  def index; end
+class Api::V1::MerchantsController < ApplicationController
+  def index
+    render json: MerchantSerializer.new(Merchant.all)
+  end
 end
