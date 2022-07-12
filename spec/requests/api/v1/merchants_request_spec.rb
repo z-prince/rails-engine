e2da224101ddd0753c77ef2fd6074c5024e05e5f
Zachary P <zachary.s.prince@outlook.com>
2022-07-12 15:56:42 -0400

    Add merchant show request test

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'The merchants API' do
-  it 'sends a list of merchants' do
+  it 'sends a list of all merchants' do
     create_list(:merchant, 3)
 
     get '/api/v1/merchants'
@@ -20,5 +20,23 @@ RSpec.describe 'The merchants API' do
       expect(merchant).to have_key(:attributes)
       expect(merchant[:attributes][:name]).to be_a(String)
     end
+  end
+
+  it 'sends the data of one merchant' do
+    merchant1 = Merchant.create!(name: 'Sal Salisbury')
+    Merchant.create!(name: 'Rip Ramwinkle')
+    Merchant.create!(name: 'Tom Bologna')
+
+    get "/api/v1/merchants/#{merchant1.id}"
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+
+    merchant = response_body[:data]
+
+    expect(response).to be_successful
+
+    expect(merchant[:attributes][:name]).to_not eq('Rip Ramwinkle')
+    expect(merchant[:attributes][:name]).to_not eq('Tom Bologna')
+    expect(merchant[:attributes][:name]).to eq('Sal Salisbury')
   end
 end
