4f5a3141207e7dfa754b56d3fc8a115734111444
Zachary P <zachary.s.prince@outlook.com>
2022-07-13 11:31:29 -0400

    Add item show route test

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -25,4 +25,24 @@ RSpec.describe 'The items API' do
       expect(item[:attributes][:unit_price]).to be_a(Float)
     end
   end
+
+  it 'sends the data of one item' do
+    merch1 = create(:merchant)
+    merch2 = create(:merchant)
+    item1 = create(:item, merchant_id: merch1.id)
+    item2 = create(:item, merchant_id: merch2.id)
+    item3 = create(:item, merchant_id: merch1.id)
+
+    get "/api/v1/items/#{item3.id}"
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+
+    merchant = response_body[:data]
+
+    expect(response).to be_successful
+
+    expect(item[:attributes][:name]).to eq(item3.name)
+    expect(item[:attributes][:description]).to eq(item3.description)
+    expect(item[:attributes][:unit_price]).to eq(item3.unit_price)
+  end
 end
