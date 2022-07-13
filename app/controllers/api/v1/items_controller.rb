1b389b7b638eeb47cc15de020db69960f00023dd
Zachary P <zachary.s.prince@outlook.com>
2022-07-13 12:05:46 -0400

    Refactor controllers to nested modules

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,19 +1,23 @@
-class Api::V1::ItemsController < ApplicationController
-  def index
-    render json: ItemSerializer.new(Item.all)
-  end
+module Api
+  module V1
+    class ItemsController < ApplicationController
+      def index
+        render json: ItemSerializer.new(Item.all)
+      end
 
-  def show
-    render json: ItemSerializer.new(Item.find(params[:id]))
-  end
+      def show
+        render json: ItemSerializer.new(Item.find(params[:id]))
+      end
 
-  def create
-    render json: Item.create(item_params)
-  end
+      def create
+        render json: Item.create(item_params)
+      end
 
-  private
+      private
 
-  def item_params
-    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+      def item_params
+        params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+      end
+    end
   end
 end
