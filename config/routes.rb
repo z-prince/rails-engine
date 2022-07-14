eb19cd164ba1a4c32307fcc51b2c752e8e216ad9
Zachary P <zachary.s.prince@outlook.com>
2022-07-14 16:50:49 -0400

    Add item merchant route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :items
+      resources :items do
+        resources :merchant, only: :index, controller: :item_merchant
+      end
       resources :merchants do
-        resources :items, controller: :merchant_items
+        resources :items, controller: :merchant_items do
+        end
       end
     end
   end
